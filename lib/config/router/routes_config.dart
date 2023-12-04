ec00993d56c64b3222266397af9502468ef5b186
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2023-12-03 20:06:19 -0500

    Add new files and icons for web and mobile platforms

---
+++ lib/config/router/routes_config.dart
@@ -0,0 +1,8 @@
+class Routes {
+  static const String home = '/';
+  static const String profile = '/profile';
+  static const String settings = '/settings';
+  static const String about = '/about';
+  static const String contact = '/contact';
+  static const String notFound = '/not-found';
+}
